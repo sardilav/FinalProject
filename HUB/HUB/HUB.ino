7e6cce87cf8cec14a8df6ace7ff89827a533d63f
ageorgewarren <ageorgewarren@gmail.com>
2018-05-13 17:53:55 -0400

    Commented the hub

--- HUB/HUB/HUB.ino
+++ HUB/HUB/HUB.ino
@@ -1,5 +1,5 @@
-// BinaryDataFromProcessing
-// These defines must mirror the sending program:
+
+
 const char HEADER       = 'H';
 const char A_TAG    = 'M';
 const char B_TAG    = 'X';
@@ -37,7 +37,9 @@ void setup()
 }
 
 void loop() {
-  if ( Serial.available() >= TOTAL_BYTES)
+  DataReceive();
+  
+  if ( Serial.available() >= TOTAL_BYTES) // Receives data from processing and sends to SendData()
   {
     if ( Serial.read() == HEADER)
     {
@@ -45,6 +47,7 @@ void loop() {
       if (tag == A_TAG)
       {
         //Collect integers
+        //Converts from ASCII to int
         int a = Serial.read() * 256;
         a = a + Serial.read();
         int b = Serial.read() * 256;
@@ -59,9 +62,17 @@ void loop() {
     }
   }
 }
+
+void StageAssign(){
+  if(A==1){ // Prints to Processing that the robot is ready
+    Serial.print("Robot #");
+    Serial.print(B);
+    Serial.println(" is ready to begin!");
+  }
+}
 //============
 
-void SendData(int Aa, int Bb, int Cc) {
+void SendData(int Aa, int Bb, int Cc) { // Sends data packet to robots
   Serial1.print('<');
   Serial1.print(Aa);
   Serial1.print(',');
@@ -74,5 +85,66 @@ void SendData(int Aa, int Bb, int Cc) {
 
 //============
 
+void DataReceive() {  
+  recvWithStartEndMarkers();          // Checks the xbee for data
+  if (newData == true) {              // if the data is new
+    strcpy(tempChars, receivedChars); // Copies data
+    parseData();                      // Parses the data into variables
+    newData = false;                  // Resets new data varaible
+  }
+}
+
+//============
+
+void recvWithStartEndMarkers() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char StartMarker = '<';
+  char endMarker = '>';
+  char rc;
+
+  while (Serial2.available() > 0 && newData == false) {   // Pulls all data from the xbee
+    rc = Serial2.read();
+
+    if (recvInProgress == true) {   // Reads data until endmarker is detected '>'
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {  // Resets index varible if too high
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        newData = true;
+      }
+    }
+
+    else if (rc == StartMarker) {   // declares that receiving data
+      recvInProgress = true;
+    }
+  }
+}
+
+//============
+
+void parseData() {
+  // split the data into its parts
+
+  char * strtokIndx; // this is used by strtok() as an index
+
+  strtokIndx = strtok(tempChars, ",");     // get the first variable 
+  A = atoi(strtokIndx);
+
+  strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+  B = atoi(strtokIndx);     // convert this part to an integer
+  
+
+  if (A > 0) {
+    StageAssign();
+  }
+}
 
 
